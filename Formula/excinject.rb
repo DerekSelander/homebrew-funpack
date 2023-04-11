ce2a56ff34a878debd0945000de078952ff67676
LOLgrep <derekselander@yahoo.com>
2023-04-11 11:55:11 -0600

    Added dwatch / excinject

---
+++ Formula/excinject.rb
@@ -0,0 +1,21 @@
+# typed: false
+# frozen_string_literal: true
+
+class Excinject < Formula
+  desc "Inject a dylib into a remote process"
+
+  depends_on :macos
+
+  on_macos do
+      url "https://github.com/DerekSelander/homebrew-funpack/archive/refs/tags/0.0.5.tar.gz"
+
+      def install
+        bin.install "excinject"
+      end
+  end
+
+
+  test do
+    system "#{bin}/excinject"
+  end
+end
