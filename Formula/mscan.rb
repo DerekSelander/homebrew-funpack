6948f5c30b12f0362647746de3a39f1f8792906c
LOLgrep <derekselander@yahoo.com>
2023-04-11 11:42:13 -0600

    Revert "Release v0.0.6"
    This reverts commit f012ae9e47cd71114857585d2178be62977d2326.

---
+++ Formula/mscan.rb
@@ -0,0 +1,21 @@
+# typed: false
+# frozen_string_literal: true
+
+class Mscan < Formula
+  desc "An LLDB plugin that aids in finding stuff in memory"
+
+  depends_on :macos
+
+  on_macos do
+      url "https://github.com/DerekSelander/homebrew-funpack/archive/refs/tags/0.0.5.tar.gz"
+
+      def install
+        bin.install "mscan"
+      end
+  end
+
+
+  test do
+    system "#{bin}/mscan"
+  end
+end
