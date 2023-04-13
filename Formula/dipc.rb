1bbb7235d6f543f88d9829f188daa7bb263fba5e
LOLgrep <derekselander@yahoo.com>
2023-04-13 15:53:03 -0600

    Release 0.0.13

--- Formula/dipc.rb
+++ Formula/dipc.rb
@@ -8,7 +8,7 @@ class Dipc < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/DerekSelander/homebrew-funpack/archive/refs/tags/0.0.12.tar.gz"
+      url "https://github.com/DerekSelander/homebrew-funpack/archive/refs/tags/0.0.13.tar.gz"
 
       def install
         bin.install "dipc"
