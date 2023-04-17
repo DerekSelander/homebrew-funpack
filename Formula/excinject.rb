3507b9f5824ea1f93f8b0ef4faa37ac49a571275
LOLgrep <derekselander@yahoo.com>
2023-04-17 16:25:50 -0600

    Release 0.0.16

--- Formula/excinject.rb
+++ Formula/excinject.rb
@@ -7,7 +7,7 @@ class Excinject < Formula
   depends_on :macos
 
   on_macos do
-      url "https://github.com/DerekSelander/homebrew-funpack/archive/refs/tags/0.0.15.tar.gz"
+      url "https://github.com/DerekSelander/homebrew-funpack/archive/refs/tags/0.0.16.tar.gz"
 
       def install
         bin.install "excinject"
