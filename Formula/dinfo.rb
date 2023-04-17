c31f0f07df3515972c2963f0173e49dcd51f71eb
LOLgrep <derekselander@yahoo.com>
2023-04-17 17:20:12 -0600

    Release 0.0.23

--- Formula/dinfo.rb
+++ Formula/dinfo.rb
@@ -8,7 +8,7 @@ class Dinfo < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/DerekSelander/homebrew-funpack/archive/refs/tags/0.0.22.tar.gz"
+      url "https://github.com/DerekSelander/homebrew-funpack/archive/refs/tags/0.0.23.tar.gz"
 
       def install
         bin.install "dinfo"
