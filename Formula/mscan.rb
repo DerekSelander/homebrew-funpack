a5b54c14df3aea3fddba67e72c649fb9493f46b4
LOLgrep <derekselander@yahoo.com>
2023-05-23 20:46:32 -0600

    Release 0.1.1

--- Formula/mscan.rb
+++ Formula/mscan.rb
@@ -7,7 +7,7 @@ class Mscan < Formula
   depends_on :macos
 
   on_macos do
-      url "https://github.com/DerekSelander/homebrew-funpack/archive/refs/tags/0.0.14.tar.gz"
+      url "https://github.com/DerekSelander/homebrew-funpack/archive/refs/tags/0.1.1.tar.gz"
 
       def install
         bin.install "mscan"
